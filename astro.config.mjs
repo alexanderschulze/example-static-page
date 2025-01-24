836792c272177870440657c74a8301a979894e5e
Alex Schulze <alexander.schulze@gmail.com>
2025-01-24 10:25:52 +0100

    Change base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://alexanderschulze.github.io',
-  base: '/example-static-page',
+  site: 'https://example-static-page.alex.zone',
+  base: '/',
   
 });
